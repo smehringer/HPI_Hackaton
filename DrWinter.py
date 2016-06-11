c55bbbd372a4aca892ed24ffcfa820bd1a074b07
MrLenLen <lennard-epping@hotmail.de>
2016-06-11 23:42:19 +0200

    validate message

--- DrWinter.py
+++ DrWinter.py
@@ -18,15 +18,33 @@ def hello_monkey():
     """Respond to incoming calls with a simple text message."""
     from_number = request.values.get('From', None)
     message=request.values.get('Body',None)
-    print message
+    valid, char = validate(message)
     if valid:
         query=prep_data(message, char)
-        deseas_class=data_predict.testData(query) 
+        deseas_class=data_predict.testData(query)
+    else:
+        message = "Hi and welcome,\n please indicate from which  of the following symptoms you are suffering from by sending me a sms containing all the numbers associated with the symptoms you can observe seperated by only commas.\nSymptoms:\n   \nThank you for using my services!\nDr. Winter"
     message = " Thanks for the message!"
     resp = twilio.twiml.Response()
     resp.message("Hello, Mobile Monkey")
     return str(resp)
 
+def validate(message_sent):
+	matchcom = re.search(r"[0-9,]+", message_sent)
+	validcom = ((matchcom.start()==0) and (matchcom.end()==len(message_sent)))
+	matchsem = re.search(r"[0-9;]+", message_sent)
+	validsem = ((matchsem.start()==0) and (matchsem.end()==len(message_sent)))
+	matchbla = re.search(r"[0-9' ']+", message_sent)
+	validbla = ((matchbla.start()==0) and (matchbla.end()==len(message_sent)))
+	
+	if validcom:
+		return [validcom, ","]
+	elif validsem:
+		return [validsem, ";"]
+	elif validbla:
+		return [validbla, " "]
+	else:
+		return [False, "u"]
 def prep_data(message,char):
     symptoms=[0]*10
     message=message.split(char)
